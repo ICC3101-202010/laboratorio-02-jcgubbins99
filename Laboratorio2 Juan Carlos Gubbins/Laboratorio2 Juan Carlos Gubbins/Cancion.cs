580232327f80b17f9d8c4e7a14af2c9ef42eeb12
Juan Carlos <juancarlos.gubbins@gmail.com>
2020-03-26 23:01:18 -0300

    thirdcommitlab02

---
+++ Laboratorio2 Juan Carlos Gubbins/Laboratorio2 Juan Carlos Gubbins/Cancion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio2_Juan_Carlos_Gubbins
+{
+    class Cancion
+    {
+        public string Nombre;
+        public string Album;
+        public string Artista;
+        public string Genero;
+        public Cancion(string _genero, string _artista, string _album, string _nombre)
+        {
+            this.Genero = _genero;
+            this.Artista = _artista;
+            this.Album = _album;
+            this.Nombre = _nombre;
+        }
+        public string information()
+        {
+            string informacion = ("Genero: " + Genero + "\n" + "Artista: " + Artista + "\n" + "Album: " + Album + "\n" + "Nombre canción: " + Nombre);
+            return informacion;
+        }
+    }
+
+}
---
+++ Laboratorio2 Juan Carlos Gubbins/Laboratorio2 Juan Carlos Gubbins/Playlist.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio2_Juan_Carlos_Gubbins
+{
+    class Playlist
+    {
+        private string NombrePlaylist;
+
+    }
+}
